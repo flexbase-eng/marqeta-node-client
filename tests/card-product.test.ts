aec64ef885286434e3ebcaf0ce102ddea478b83e
jb <john.bearden@buildxcorp.com>
2022-05-03 17:35:50 -0400

    marqeta-client-card-product-get: byTokenId() function with tests

--- tests/card-product.test.ts
+++ tests/card-product.test.ts
@@ -11,12 +11,24 @@ import { Marqeta } from '../src';
 
   if (client) {
     console.log('Success! Client initialized')
-    console.log('getting Card Products...')
+    console.log('getting Marqeta Card Products...')
 
     const products = await client.cardProduct.list()
 
     if (products?.success) {
-      console.log('Success! We were able to get the Card Products')
+      console.log('Success! We were able to get the Marqeta Card Products')
+      const lstItem1 = products?.body?.data?.pop()
+
+      if (lstItem1?.token) {
+        console.log(`getting Marqeta Card Product by id: ${lstItem1.token}`)
+        const fouA = await client.cardProduct.byTokenId(lstItem1.token)
+
+        if (fouA?.body?.token) {
+          console.log('Success! The Marqeta Card Product was found by id: ' +
+            JSON.stringify(fouA.body.token))
+        }
+      }
+
     } else {
       console.log('Error! Could not get any Card Products')
     }
