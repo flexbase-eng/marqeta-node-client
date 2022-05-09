2253d96718b4e8a7088b16188a8b70364d302502
jb <john.bearden@buildxcorp.com>
2022-05-09 08:32:26 -0400

    marqeta-client-webhooks-list: webhooks module list() function.

--- tests/webhooks.test.ts
+++ tests/webhooks.test.ts
@@ -47,4 +47,19 @@ import { Marqeta } from '../src';
     console.log(list)
   }
 
+  console.log('getting a list of onew Webhooks...')
+  const listOne = await client.webHooks.list({ count: 1 })
+
+  if (listOne.body?.data && Array.isArray(listOne.body?.data)) {
+    if(listOne.body.data.length === 1){
+      console.log(`Success! One ${listOne.body!.count} Webhook was retrieved.`)
+    } else {
+      console.log('Error! Unable to get a list of one Webhook.')
+      console.log(listOne)
+    }
+  } else {
+    console.log('Error! Unable to get a list Webhooks.')
+    console.log(listOne)
+  }
+
 })()
