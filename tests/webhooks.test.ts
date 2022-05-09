e24b6ed5aa736c00b8bde057b35df212cf91b9cb
jb <john.bearden@buildxcorp.com>
2022-05-09 08:35:40 -0400

    marqeta-client-webhooks-list: lint fix.

--- tests/webhooks.test.ts
+++ tests/webhooks.test.ts
@@ -9,39 +9,11 @@ import { Marqeta } from '../src';
     apiAccessToken: process.env.MARQETA_API_ACCESS_TOKEN
   })
 
-  const mockWebhook = {
-    businessNameLegal: 'AcmeZinc INC',
-    businessNameDba: 'zinc inc',
-    incorporation: {
-      stateOfIncorporation: 'LA',
-      incorporationType: 'CORPORATION'
-    },
-    proprietorOrOfficer: {
-      firstName: '',
-      lastName: '',
-      home: {
-        address1: '106 Main St.',
-        address2: '',
-        city: 'Opelousas',
-        state: 'LA',
-        postalCode: '70570',
-        country: 'USA'
-      }
-    },
-    identifications: [
-      {
-        type: 'BUSINESS_TAX_ID',
-        value: '901721634'
-      }
-    ],
-  }
-
   console.log('getting a list of Webhooks...')
   const list = await client.webHooks.list()
 
   if (list.body?.isMore) {
     console.log(`Success! ${list.body!.count} Webhooks were retrieved.`)
-    //const lstItem1 = list?.body?.data?.pop()
   } else {
     console.log('Error! Unable to get a list of Webhooks.')
     console.log(list)
