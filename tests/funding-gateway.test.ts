1fcaeceee61102119d3715a4542d6a2f27a70adc
jb <john.bearden@buildxcorp.com>
2022-05-13 14:59:44 -0400

    marqeta-funding-gateway: fixed insecure math random codeql error

--- tests/funding-gateway.test.ts
+++ tests/funding-gateway.test.ts
@@ -10,11 +10,11 @@ import { Marqeta } from '../src'
   })
 
   const mockSource = {
-    name: 'funding_gate_way_test_' + Math.floor(Math.random() * 100) + 1,
+    name: 'funding_gate_way_test_' + Math.floor(65536 * 100) + 1,
     url: 'https://www.ipsumlorem.com',
     token: '',
     basicAuthUsername: 'ipsum_lorem',
-    basicAuthPassword: 'abbykj26stringzIl2-Mzk' + Math.floor(Math.random() * 100) + 1,
+    basicAuthPassword: 'abbykj26stringzIl2-Mzk' + Math.floor(65536 * 100) + 1,
     customHeader: { 'X_header_name' : 'x_header_value' },
   }
 
