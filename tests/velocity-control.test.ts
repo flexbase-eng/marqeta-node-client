8eb9383c38036aab8b6e6d9b73f1402fee2e30b7
jb <john.bearden@buildxcorp.com>
2022-05-18 15:42:28 -0400

    marqeta-veloticy-control: initial velocity control module with create() function

---
+++ tests/velocity-control.test.ts
@@ -0,0 +1,48 @@
+'use strict'
+
+import { Marqeta } from '../src'
+(async () => {
+
+  const client = new Marqeta({
+    host: process.env.MARQETA_HOST,
+    apiAppToken: process.env.MARQETA_API_APP_TOKEN,
+    apiAccessToken: process.env.MARQETA_API_ACCESS_TOKEN
+  })
+
+  const mockVelocityControl = {
+    usage_limit: 10,
+    amount_limit: 500,
+    velocity_window: 'DAY',
+    association: {
+      userToken: ''
+    },
+    currency_code: 'USD',
+    token: ''
+  }
+
+  console.log('getting a user ...')
+  let user
+  const userList = await client.user.list({ count: 1 })
+
+  if (userList?.body?.isMore && Array.isArray(userList?.body?.data)) {
+    user = userList.body.data.pop()
+    if (user?.token) {
+      mockVelocityControl.association.userToken = user.token
+      const control = await client.velocityControl.create(mockVelocityControl)
+      if (control.success && control?.body?.token) {
+        console.log('Success! A velocity control was created for user: ' +
+          user?.token)
+      } else {
+        console.log('Success! Unable to create a velocity control for user: ' +
+          + user?.token)
+        console.log(control)
+      }
+    } else {
+      console.log('Error! Empty user token Id.')
+    }
+  } else {
+    console.log('Error! Unable to get a list of users.')
+    console.log(userList)
+  }
+
+})()
