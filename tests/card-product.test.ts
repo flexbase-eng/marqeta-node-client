cc2df8a919d0b057fa11367085b62cfc051f468e
jb <john.bearden@buildxcorp.com>
2022-06-02 13:48:42 -0400

    marqeta-cardproducts-test-refactor

--- tests/card-product.test.ts
+++ tests/card-product.test.ts
@@ -14,84 +14,66 @@ import { Marqeta } from '../src';
     'startDate': '2019-08-24T14:15:22Z'
   }
 
-  if (client) {
-    console.log('Success! Client initialized')
-    console.log('getting Marqeta Card Products...')
+  console.log('creating Card Product...')
+  const cardProduct = await client.cardProduct.create(mockCardProduct)
 
-    const products = await client.cardProduct.list()
-    const listItem1 = products?.cardProducts?.data?.pop()
-
-    console.log('testing getting list of Marqeta Card Products...')
-    if (listItem1?.token) {
-      console.log('Success! We were able to get Marqeta Card Products')
-      console.log(`getting Marqeta Card Product by id: ${listItem1.token}`)
-      const fouA = await client.cardProduct.byTokenId(listItem1.token)
-
-      if (fouA?.cardProduct?.token) {
-        console.log('Success! The Marqeta Card Product was found by id: ' +
-            JSON.stringify(fouA.cardProduct.token))
-        let upA
-        const fouAName = fouA.cardProduct.name
-
-        if (fouA.cardProduct?.name) {
-          fouA.cardProduct.name += Math.floor(Math.random() * 100) + 1
-          upA = await client.cardProduct.update(fouA.cardProduct)
-        }
-
-        if (upA?.success) {
-          console.log('Success! The Card Product  name was updated from "' +
-              fouAName + '" to "' + upA.cardProduct?.name + '"')
-        } else {
-          console.log('Error! Unable to update the Card Product ')
-          console.log(upA)
-        }
-      }
-    } else {
-      console.log('Error! Could not get a list of Card Products')
-    }
-
-    console.log('testing creating Marqeta Card Product...')
-    const cardProductA = await client.cardProduct.create(mockCardProduct)
-
-    if (cardProductA?.success && cardProductA?.cardProduct?.token) {
-      console.log('Success! The Card Product. "' +
-          cardProductA.cardProduct?.name +
-          '" was created with token: ' + cardProductA.cardProduct?.token)
-      console.log('getting Card Product by id: ' + cardProductA.cardProduct?.token)
-      const foundB = await client.cardProduct.byTokenId(cardProductA.cardProduct?.token)
-
-      if (foundB.success && foundB?.cardProduct?.token) {
-        console.log('Success! The Card Product  "' +
-            cardProductA.cardProduct?.name + '" was found with Card Product id: ' +
-            cardProductA?.cardProduct?.token)
-        console.log('updating Card Product name...')
-        const orgNameB = foundB.cardProduct.name
-        let updatedB
-
-        if (foundB.cardProduct.name) {
-          foundB.cardProduct.name += Math.floor(Math.random() * 100) + 1
-          updatedB = await client.cardProduct.update(foundB.cardProduct)
-        }
-
-        if (updatedB?.cardProduct?.token) {
-          console.log('Success! The Card Product name was updated from "' +
-              orgNameB + '" to "' + updatedB.cardProduct?.name + '"')
-        } else {
-          console.log('Error! Unable to update the Card Product')
-          console.log(updatedB)
-        }
-      } else {
-        console.log('Error! Unable to get Card Products by id.')
-        console.log(foundB)
-      }
-
-    } else {
-      console.log('Error! Unable to create a Card Product.')
-      console.log(cardProductA)
-    }
-
+  if (cardProduct?.success && cardProduct?.cardProduct?.token) {
+    console.log('Success! Card Product created.')
   } else {
-    console.log('Error! Could not create a Marqeta Client instance')
+    console.log('Error! Unable to create a Card Product.')
+    console.log(cardProduct)
+  }
+
+  console.log('retrieving Card Product...')
+
+  if (cardProduct?.cardProduct?.token) {
+    const foundB = await client.cardProduct.retrieve(
+      cardProduct.cardProduct?.token
+    )
+
+    if (foundB.success && foundB?.cardProduct?.token) {
+      console.log('Success! Found Card Product by token Id.')
+    } else {
+      console.log('Error! Unable to get Card Product by token Id.')
+      console.log(foundB)
+    }
+  } else {
+    console.log('Error! Empty Card Product token Id.')
+    console.log(cardProduct)
+  }
+
+  console.log('updating Card Product...')
+
+  if (cardProduct?.cardProduct?.token) {
+    const orgName = cardProduct.cardProduct.name
+    let updated
+
+    if (cardProduct.cardProduct.name) {
+      cardProduct.cardProduct.name += Math.floor(Math.random() * 100) + 1
+      updated = await client.cardProduct.update(cardProduct.cardProduct)
+    }
+
+    if (updated?.cardProduct?.token) {
+      console.log('Success! The Card Product name was updated from "' +
+          orgName + '" to "' + updated.cardProduct?.name + '"')
+    } else {
+      console.log('Error! Unable to update Card Product')
+      console.log(updated)
+    }
+  } else {
+    console.log('Error! Empty Card Product token Id.')
+    console.log(cardProduct)
+  }
+
+  console.log('getting list of Card Products...')
+  const products = await client.cardProduct.list()
+
+  if (Array.isArray(products?.cardProducts?.data)
+         && products?.cardProducts?.count) {
+    console.log('Success! Retrieved a list of Card Products.')
+  } else {
+    console.log('Error! Unable to retrieve a list of Card Products.')
+    console.log(products)
   }
 
 })()
