3a94dd28720842160f70c6983947277e1519d3e6
jb <john.bearden@buildxcorp.com>
2022-06-06 17:18:30 -0400

    marqeta-program-funding-update

--- tests/funding-program.test.ts
+++ tests/funding-program.test.ts
@@ -14,6 +14,8 @@ import { Marqeta } from '../src'
     active: true,
   }
 
+  let found, updated
+
   console.log('getting list of one Marqeta user...')
   const fund = await client.fundingProgram.create(mockFunding)
 
@@ -25,7 +27,7 @@ import { Marqeta } from '../src'
   }
 
   if (fund?.funding?.token) {
-    const found = await client.fundingProgram.retrieve(fund.funding.token)
+    found = await client.fundingProgram.retrieve(fund.funding.token)
 
     if (found.success) {
       console.log('Success! Program Funding source found.')
@@ -33,7 +35,23 @@ import { Marqeta } from '../src'
       console.log('Error! Program Funding source not found.')
       console.log(found)
     }
+  } else {
+    console.log('Error! Empty funding program token.')
+    console.log(fund)
+  }
 
+  if (found?.funding?.name && found?.funding?.token) {
+    const originalName = found.funding.name
+    found.funding.name = 'updated-funding-test'
+    updated = await client.fundingProgram.update(found.funding)
+
+    if (updated.success && updated?.funding?.name) {
+      console.log('Success! Program Funding source name updated from "' +
+        originalName + '" to "' + updated.funding.name + '"')
+    } else {
+      console.log('Error! Program Funding source not updated.')
+      console.log(updated)
+    }
   }
 
 })()
